61986b7996cf075890060a29118f66f5d845ef12
randykuo <randy_chiang@yahoo.com>
2019-06-03 14:52:08 +0800

    commit all the modified files

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World open shift 君品研討會!'); //write a response to the client
+	  var html = [
+        '<!DOCTYPE html>',
+        '<html>',
+            '<head>',
+                '<meta charset="utf-8" />',
+                '<title>openshift demo</title>',
+            '</head>',
+            '<body>',
+                '<p>Hello World openshift 君品研討會!</p>',
+            '</body>',
+        '</html>'
+    ].join('');
+  res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+  res.write(html); //write a response to the client
   res.end(); //end the response
 }).listen(8080); //the server object listens on port 8080
