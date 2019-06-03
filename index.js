5f6069a5e5753a6d7617f59ef2eda364c42c9a9d
randykuo <randy_chiang@yahoo.com>
2019-06-03 13:29:33 +0800

    commit all the modified files

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(80); //the server object listens on port 8080
